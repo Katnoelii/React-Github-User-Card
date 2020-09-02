4c5027ebed109141f61a31c066a18580e3faf3bf
kate roy <kroy15chs@gmail.com>
2020-09-02 01:08:45 -0400

    MVP

--- react-usercard/src/App.js
+++ react-usercard/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
+import UserCard from './components/CardMaker'
+import Followers from './components/Followers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userData: {}
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/katnoelii')
+      .then(res => {
+        console.log('my data', res.data)
+        this.setState({
+          userData: res.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Github Friends</h1>
+        <UserCard userData={this.state.userData}/>
+        <Followers />
+      </div>
+      
+    )
+  }
 }
 
 export default App;
